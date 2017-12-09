85f4416150181e3549866ef4d81ca040764917e7
student <student@MAC03>
2017-12-09 11:16:49 +0200

    Classworkmid

---
+++ 6.1 (multiplication tab)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._1__multiplication_tab_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+
+            for (int i = 2; i <= 9; i++)
+            {
+                for (int j = 2; j <= 9; j++)
+                {
+                    Console.SetCursorPosition(x, y + j);
+                    Console.WriteLine($"{i} x {j} = {i * j,2}");
+                }
+                x += 15;
+                if (i == 5)
+                    Console.SetCursorPosition(x = 0, y = 9);
+
+            }
+        }
+    }
+}
--- Classwork 9 dec 17/Program.cs
+++ Classwork 9 dec 17/Program.cs
@@ -10,6 +10,42 @@ namespace Classwork_9_dec_17
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a number:");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= number; i++)
+            {
+                for (int j = 0; j <= number; j++)
+                {
+                    if (i == 0 || i == number - 1)
+                        Console.Write('*');
+                    else if (j == 0 || j == number - 1)
+                        Console.Write('*');
+                    else
+                        Console.Write(' ');
+                }
+                Console.WriteLine();
+            }
+            //}
+            //int repeat = number;
+            //while (repeat > 0)
+            //{
+            //    for (int j = 0; j <= number; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //    repeat--;
+            //}
+            //for (int i = number; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
         }
     }
 }
+
+
